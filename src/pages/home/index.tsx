38072754168d8a662506a5711e1c60b6a92f187c
caixianwen <caixianwen1@shein.com>
2022-03-15 16:15:11 +0800

    代码整理测试

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,25 @@
-import request from "@/utils/request";
-import React from "react";
-import styles from "./index.less";
+import request from '@/utils/request';
+import React from 'react';
+import styles from './index.less';
 const Home = () => {
-  const onUpload = (res) => {
-    // console.log();
-    const file = res.target.files[0];
-    let formData = new FormData();
-    formData.append("file", file);
-    request({
-      url: "/upload",
-      method: "post",
-      data: formData,
-    });
-  };
-  return (
-    <div className={styles.home}>
-      <input type="file" name="filename" onChange={onUpload} />
-    </div>
-  );
+    const onUpload = res => {
+        // console.log();
+        const file = res.target.files[0];
+        let formData = new FormData();
+        formData.append('file', file);
+        let a = [1, 3, 4, 5];
+        request({
+            url: '/upload',
+            method: 'post',
+            data: formData
+        });
+    };
+
+    return (
+        <div className={styles.home}>
+            <input type="file" name="filename" onChange={onUpload} />
+        </div>
+    );
 };
 
 export default Home;
